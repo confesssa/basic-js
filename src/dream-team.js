351374a456408dc669f7f824357b97bfed24e127
volha <v.khviadchenia@gmail.com>
2020-09-28 01:56:43 +0200

    feat: add solution for dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+//   if (Array.isArray(members)) {
+//     let sum = []; // вопрос
+//     for (let i = 0; i < members.length; i++) {
+//       if (typeof(members[i]) === 'string') {
+//         sum.push(members[i]);
+//       };
+//     };
+//     sum.sort();
+
+//     let result = [];
+//     for (let i = 0; i < result.length; i++) {
+//       let item = sum[i].trim();
+//       result.push(item);
+//     }
+//     return String(result).toUpperCase;
+//   } else {
+//     return false;
+//   }
+// };
+
+if (Array.isArray(members)) {
+  let result1 = [];
+  for (let i=0; i < members.length; i++) {
+    if (typeof members[i] === 'string') {
+      result1.push(members[i].trim());
+    }
+  };
+  let result2 = [];
+  for (let j=0; j < result1.length; j++ ) {
+    result2.push(result1[j][0]);
+  }
+  return String(result2.sort().join('')).toUpperCase();
+
+} else {
+  return false;
 };
+}
