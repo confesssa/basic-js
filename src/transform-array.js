769427ca5521c5cb66341c5a5eb2052e3afb3c60
volha <v.khviadchenia@gmail.com>
2020-09-30 04:24:42 +0200

    feat: add solution for transfor-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new RangeError;
+  } else if (arr.length === 0) {
+    return [];
+  } else {
+    // let result = arr.slice();
+    // for (let i = 0; i < arr.length; i++) {
+    //   if (typeof(result[i]) === 'number') {
+    //     result.push(arr[i]);
+    //   } else if (result[i] === '--discard-next') {
+    //     result.splice(i + 1, 1)
+    //   } else if (result[i] === '--discard-prev') {
+    //     result.splice(i - 1, 1);
+    //   }
+    // }
+    
+  }
 };
