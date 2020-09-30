b17c361e1ca22246caf69fcbcefa282334c01751
volha <v.khviadchenia@gmail.com>
2020-09-30 03:00:09 +0200

    fix: update solution for what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,20 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-  if (date === null) {
+  if (!!date === false) {
     return 'Unable to determine the time of year!';
   } else if (date.getMonth) {
-    if (date.getMonth === 0 || date.getMonth === 1 || date.getMonth === 11) {
-      return 'winter';
-    } else if (date.getMonth === 2 || date.getMonth === 3 || date.getMonth === 4) {
-      return 'spring';
-    } else if (date.getMonth === 5 || date.getMonth === 6 || date.getMonth === 7) {
-      return 'summer';
-    } else {
-      return 'autumn';
+    let seasons = ['winter', 'spring', 'summer', 'autumn'];
+    if (date.getMonth() === 0 || date.getMonth() === 1 || date.getMonth() === 11) {
+      return seasons[0];
+    } else if (date.getMonth() === 2 || date.getMonth() === 3 || date.getMonth() === 4) {
+      return seasons[1];
+    } else if (date.getMonth() === 5 || date.getMonth() === 6 || date.getMonth() === 7) {
+      return seasons[2];
+    } else if (date.getMonth() === 8 || date.getMonth() === 9 || date.getMonth() === 10) {
+      return seasons[3];
+    } {
+      throw new RangeError;
     }
 
   } else {
-    return 'ERROR';
+    throw new RangeError(Error);
   }
 };
