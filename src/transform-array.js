8a8a70796a46fad4768df7dcbd46fff5086a731b
volha <v.khviadchenia@gmail.com>
2020-10-11 01:00:21 +0200

    feat: add part of solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -6,16 +6,46 @@ module.exports = function transform(arr) {
   } else if (arr.length === 0) {
     return [];
   } else {
-    // let result = arr.slice();
-    // for (let i = 0; i < arr.length; i++) {
-    //   if (typeof(result[i]) === 'number') {
-    //     result.push(arr[i]);
-    //   } else if (result[i] === '--discard-next') {
-    //     result.splice(i + 1, 1)
-    //   } else if (result[i] === '--discard-prev') {
-    //     result.splice(i - 1, 1);
-    //   }
-    // }
+
+    let result = [];
     
+    for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'number') {
+    if (arr[i - 1] !== '--discard-next') {
+    result.push(arr[i]);
+    } else {
+    result.push('');
+    }
+    } else if (arr[i] === '--double-next') {
+    if (arr.indexOf('--double-next')  === (arr.length - 1)) {
+    result[i] = ''; // возможно нужно будет изменить
+    } else {
+    result[i] = arr[i + 1];
+    };
+    } else if (arr[i] === '--double-prev') {
+    if (arr.indexOf('--double-prev')  === 0) {
+    result[i] = '';
+    } else {
+    result[i] = arr[i - 1];
+    };
+    }  else if (arr[i] === '--discard-prev') {
+    if (arr.indexOf('--discard-prev')  === 0) {
+    result[i] = '';
+    } else {
+    result = result.slice(0, -1);
+    };
+    } else if (arr[i] === '--discard-next') {
+    result[i] = '';
+    } 
+    }// конец цикла
+    
+    
+    
+    let result2 = result.filter(function(el) {
+    return typeof el === 'number';
+    }); 
+    
+    
+return result2;
   }
 };
