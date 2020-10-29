fb09ec5165c85792aa25989025bb036db93afd0c
volha <v.khviadchenia@gmail.com>
2020-10-30 00:39:52 +0100

    feat: add solution for recursive depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,24 +1,13 @@
 const CustomError = require("../extensions/custom-error");
-let a;
-let b;
-let count;
 module.exports = class DepthCalculator {
 
-
   calculateDepth(arr) {
-    a = arr;
-    b = a.flat();
-    count = 1;
-    if (Array.isArray(a) &&
-    Array.isArray(b) &&
-    a.length === b.length &&
-    a.every((val, index) => val === b[index])) {
-return count;
-} else {
-count++;
-return calculateDepth(b, b.flat());
-};
-
-
-  };
+    if (arr.some(function(el) {
+      return Array.isArray(el);
+    })) {
+      return 1 + this.calculateDepth(arr.flat());
+    } else {
+      return 1;
+    }
+  }
 };
