d18dc5388e57725fefe79d9228104015e7ba46d3
volha <v.khviadchenia@gmail.com>
2020-10-30 21:51:35 +0100

    feat: add solution for Transform Array

--- src/transform-array.js
+++ src/transform-array.js
@@ -5,47 +5,34 @@ module.exports = function transform(arr) {
     throw new RangeError;
   } else if (arr.length === 0) {
     return [];
-  } else {
-
-    let result = [];
-    
-    for (let i = 0; i < arr.length; i++) {
-    if (typeof arr[i] === 'number') {
-    if (arr[i - 1] !== '--discard-next') {
-    result.push(arr[i]);
-    } else {
-    result.push('');
-    }
-    } else if (arr[i] === '--double-next') {
-    if (arr.indexOf('--double-next')  === (arr.length - 1)) {
-    result[i] = ''; // возможно нужно будет изменить
-    } else {
-    result[i] = arr[i + 1];
-    };
-    } else if (arr[i] === '--double-prev') {
-    if (arr.indexOf('--double-prev')  === 0) {
-    result[i] = '';
-    } else {
-    result[i] = arr[i - 1];
-    };
-    }  else if (arr[i] === '--discard-prev') {
-    if (arr.indexOf('--discard-prev')  === 0) {
-    result[i] = '';
-    } else {
-    result = result.slice(0, -1);
-    };
-    } else if (arr[i] === '--discard-next') {
-    result[i] = '';
-    } 
-    }// конец цикла
-    
-    
-    
-    let result2 = result.filter(function(el) {
-    return typeof el === 'number';
-    }); 
-    
-    
-return result2;
   }
+
+let result = [];
+for (let i = 0; i < arr.length; ++i) {
+    switch (arr[i]) {
+        case '--double-next' :
+            if (i !== arr.length - 1) {
+                result.push(arr[i + 1])
+            }
+            break;
+        case '--double-prev' :
+            if (i > 0 && arr[i - 2] !== '--discard-next') {
+                result.push(arr[i - 1])
+            }
+            break;
+        case '--discard-next' :
+            if (i !== arr.length) {
+               i++
+            }
+            break;
+        case '--discard-prev' :
+            if (i > 0 && arr[i - 2] !== '--discard-next') {
+                result.pop();
+            }
+            break;
+        default:
+            result.push(arr[i]);
+    }
+}
+return result
 };
